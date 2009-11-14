fcef47ce838b54f218e62ade8f80a303514cd8cb
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-14 20:49:17 +0000

    fix rstable( alpha = 1, beta != 0)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/rstable1.R
+++ pkg/R/rstable1.R
@@ -1,41 +1,80 @@
-rstable1 <- function(n, alpha, beta, gamma = 1, delta = 0)
+##' @param x  numeric vector
+
+##' @return tan(pi *x)	but exact for integer x
+tanpi <- function(x) {
+    r <- x
+    if(any(i <- x == round(x)))
+	r[i] <- 0
+    io <- which(!i)
+    r[io] <- tan(pi* x[io])
+    r
+}
+
+
+rstable1 <- function(n, alpha, beta, gamma = 1, delta = 0, pm = 1)
 {
-    ## Purpose: Random Numbers of Stable distribution in "1 - parametrization"
+    ## Purpose: Random Numbers of Stable distribution in
+    ## "0"- or "1" - parametrization
     ## ----------------------------------------------------------------------
     ## Author: Martin Maechler, based on Diethelm Wuertz's code in fBasics
 
-    stopifnot(0 < alpha, alpha <= 2,
-              -1 <= beta, beta <= 1)
+    ## TODO: Want this to *vectorize* in  alpha, beta, gamma, delta
 
-    ## if(pm == 1) ##  "S" aka "S1" or "1" Parametrization
-    ## [[ if you drop this, you have the "S0" or "0" parametrization ]]
-    delta <- delta + beta * gamma *
-        ifelse(alpha == 1, (2/pi)*log(gamma), tan(pi*alpha/2))
+    stopifnot((la <- length(alpha)) >= 1, (lb <- length(beta)) >= 1,
+	      length(gamma) >= 1, length(delta) >= 1,
+	      0 < alpha, alpha <= 2, abs(beta) <= 1,
+	      length(pm) == 1, pm %in% 0:1)
 
+    p2 <- pi/2
     # Special case (a,b) = (1,0):
     if (all(alpha == 1) && all(beta == 0)) {
-        res <- rcauchy(n)
+	Z <- rcauchy(n)
     }
     else {
-        ## Calculate uniform and exponential distributed random numbers:
-        theta <- pi * (runif(n)-1/2)
-        w <- rexp(n)
-        ##   ^^^^^^ was "-log(runif(n))"; rexp() is faster, giving different numbers
+	## MM: Nolan(2009) "chapt1.pdf", p.21 has  "Theorem 1.19"
+	## -- and attributes that to  'Chambers et al. (1976)'
 
-        ## FIXME: for alpha == 1, b.tan.pa <- Inf , but that needs different code
-        ##        N.B.: Do this such that it vectorizes in alpha, too
-        ## FIXME(2): ditto for    | alpha - 1 | << 1
-        b.tan.pa <- beta*tan(pi*alpha/2)
-        c. <- (1 + b.tan.pa^2)^(1/(2*alpha))
-        th0 <- atan(b.tan.pa) / alpha
-        a.tht <- alpha*(theta+th0)
-        res <- c.* sin(a.tht) / cos(theta)^(1/alpha) *
-            (cos(theta-a.tht)/w)^((1-alpha)/alpha)
+	## Calculate uniform and exponential distributed random numbers:
+	Theta <- pi * (runif(n)-1/2)
+	W <- rexp(n)
+	##   ^^^^^^ was "-log(runif(n))"; rexp() is faster, giving different numbers
 
-        res <- res - b.tan.pa
+	norm <- alpha != 1 ## TODO:  abs(alpha - 1) > eps.alpha1
+	## FIXME(2): ditto for	  | alpha - 1 | << 1
+
+	Z <- numeric(n)
+	if(any(norm)) { ## alpha != 1
+	    alp <- alpha[norm]; Thet <- Theta[norm]
+	    b.tan.pa <- beta[norm]*tanpi(alp/2)
+	    th0 <- atan(b.tan.pa) / alp ## == \theta_0
+	    ## Now, from Nolan/Chamber's formula, we replace
+	    ##	1/(\cos(\alpha\theta_0) \cos\Theta)^{1/\alpha}	with
+	    ## c / (\cos\Theta)^{1/\alpha}  where
+	    ## c := (1 + \beta*\tan(\pi\alpha/2))^{1/{2\alpha}}
+	    ## need to show that c = 1/(\cos(\alpha\theta_0))^{1/\alpha}
+	    ## <==> 1 + \beta*\tan(\pi\alpha/2) = 1 / (\cos(\alpha\theta_0))^2
+	    c. <- (1 + b.tan.pa^2)^(1/(2*alp))
+	    a.tht <- alp*(Thet+th0)
+	    Z[norm] <-
+		sin(a.tht) * c. / cos(Thet)^(1/alp) *
+		    (cos(a.tht-Thet)/W[norm])^((1-alp)/alp) - b.tan.pa
+	}
+        ## {note that logicals vectorize, indices do *not* so easily}
+	if(any(a1 <- !norm)) { ## alpha == 1
+	    bet <- beta[a1]; Thet <- Theta[a1]
+	    p2 <- pi/2
+	    p2.bt <- p2 + bet*Thet
+	    Z[a1] <- (p2.bt*tan(Thet) -
+		      bet*log((p2*W[a1]*cos(Thet))/p2.bt))/p2
+	}
     }
 
-    # Result:
-    res * gamma + delta
+    if(pm == 1) ##  "S" aka "S1" or "1" Parametrization
+	delta <- delta + beta * gamma *
+	    ifelse(alpha == 1, log(gamma)/p2, tanpi(alpha/2))
+    ## else pm == 0 is what we use
+
+    # Result: Location - Scale trafo -- only now using (gamma, delta):
+    Z * gamma + delta
 }
 
