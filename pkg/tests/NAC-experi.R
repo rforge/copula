ff49b24424e20d9db993065b1c837fb441023cf0
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-09 13:03:52 +0000

    used Clayton's family in NAC-experi.R; computed the value by hand => value() seems to work :-)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/NAC-experi.R
+++ pkg/tests/NAC-experi.R
@@ -2,10 +2,10 @@ library(nacopula)
 
 ## Manual construction
 c3 <-
-    new("final_nACopula", copula = setTheta(copAMH, 1/2),
+    new("final_nACopula", copula = setTheta(copClayton, 0.5),
         comp = as.integer( 1 ),
         childCops = list(new("nACopula",
-                             copula = setTheta(copAMH, 0.4),
+                             copula = setTheta(copClayton, 2),
                              comp = as.integer(c(2,3)))) # and no childCops
         )
 
@@ -16,6 +16,6 @@ stopifnot(d == 3,
 
 
 #====test value function====
-value(c3, u = c(.3, .4, .5))
+value(c3, u = c(.3, .4, .5))#correct value; should be 1/(((1/u2)^2+(1/u3)^2-1)^(1/4)-1+1/sqrt(u1))^2=0.1514384; is correct!
 
 
