e4153211ca9c99790265b7cf9fce61de8a14c89b
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-22 17:11:41 +0000

    fixed obvious error in nacopula.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/nacopula.R
+++ pkg/R/nacopula.R
@@ -26,10 +26,10 @@ setMethod("rn", signature(x ="final_nACopula"),
 	      Cout <- x@copula#outer copula
 	      theta <- Cout@theta#theta for outer copula
 	      V0 <- Cout@V0(n,theta)#generate V0's
-	      childdat <- lapply(x@childCops,rnchild)#start recursion
+	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cout@psiInv,theta0=theta,V0=V0)#start recursion
 	      dns <- length(x@comp)#dimension of the non-sectorial part
         r <- matrix(runif(n*dns),nrow=n,ncol=dns)#generate the non-sectorial part
-        mat <- cbind(r, do.call(cbind, childdat$U))#put pieces together
+        mat <- cbind(r, do.call(cbind,childdat$U))#put pieces together
         dat <- Cout@psi(-log(mat)/V0,theta=theta)#transform
         j <- c(x@comp,childdat$indCol)#get correct sorting order
         Cout@psi(dat[,j],theta)#permute data and return
@@ -43,7 +43,7 @@ setMethod("rnchild", signature(x ="nACopula"),
     function(x,n,psi0Inv,theta0,V0) {
         Cin <- x@copula#inner copula
 	      theta1 <- Cin@theta#theta_1 for inner copula
-	      V01 <- Cin@V01(n,theta0,theta1)#generate V01's
+	      V01 <- Cin@V01(V0,theta0,theta1)#generate V01's
 	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cin@psiInv,theta0=theta1,V0=V01)#recursion
 	      dns <- length(x@comp)#dimension of the non-sectorial part
         r <- matrix(runif(n*dns),nrow=n,ncol=dns)#generate the non-sectorial part
