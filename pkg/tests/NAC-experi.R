8a7da11b04465d2bf5554be9f7ee6b7be981b771
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-22 17:10:46 +0000

    added tests of rn(). Currently not working [do.call() in nacopula.R contains an error]
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@42 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/NAC-experi.R
+++ pkg/tests/NAC-experi.R
@@ -1,29 +1,31 @@
 library(nacopula)
 
-## Manual construction
+## 3d Clayton copula example
 c3 <-
     new("final_nACopula", copula = setTheta(copClayton, 0.5),
         comp = as.integer( 1 ),
         childCops = list(new("nACopula",
                              copula = setTheta(copClayton, 2),
-                             comp = as.integer(c(2,3)))) # and no childCops
+                             comp = as.integer(c(2,3)))) # no childCops
         )
 
+## basic check
 d <- dim(c3)
 stopifnot(d == 3,
 	  allComp(c3) == 1:3,
 	  allComp(c3@childCops[[1]]) == 2:3)
 
-
-##==== test value() ====
+## test value()
 (v <- value(c3, u = c(.3, .4, .5))) # 0.1514383947453
 stopifnot(all.equal(v,
  local({u1 <- .3; u2 <- .4; u3 <- .5;
         1/((1/u2^2 +1/u3^2 -1)^(1/4) -1 +1/sqrt(u1))^2}),
                     tol = 1e-14))
-                                       
+                         
+## test rn()
+rn(c3,100)              
 
-## Manual construction of the "mean example"
+## 9d Clayton copula example
 c9 <- new("final_nACopula", copula = setTheta(copClayton, 0.5),
          comp = as.integer(c(3,6,1)),
          childCops = list(new("nACopula",
@@ -31,24 +33,23 @@ c9 <- new("final_nACopula", copula = setTheta(copClayton, 0.5),
                              comp = as.integer(c(9,2,7,5)),
                              childCops = list(new("nACopula",
                                                   copula = setTheta(copClayton, 3),
-                                                  comp = as.integer(c(8,4)) # and no childCops
+                                                  comp = as.integer(c(8,4)) # no childCops
                                                  )
                                              )
                             )
                         )  
          )
 
+## basic check
 d <- dim(c9)
 stopifnot(d == 9,
-    allComp(c9) == c(3,6,1,9,2,7,5,8,4),#output of allComp(c9): 3 6 1 9 2 7 5 8 4
-    allComp(c9@childCops[[1]]) == c(9,2,7,5,8,4),##FIXME: the output of c9@childCops[[1]] is c(9,2,7,5,8,4) but then allComp(c9) == 1:9 is not fulfilled anymore
+    allComp(c9) == c(3,6,1,9,2,7,5,8,4),
+    allComp(c9@childCops[[1]]) == c(9,2,7,5,8,4),
     allComp(c9@childCops[[1]]@childCops[[1]]) == c(8,4))
 
-
-##==== test value() ====
+## test value()
 u <- seq(0.1,0.9,by=0.1)
-#with value function
-v <- value(c9, u)
+v <- value(c9, u)#with value function
 #by hand
 psi = function(t,theta) { (1+t)^(-1/theta) }
 psiInv = function(t,theta) { t^(-theta) - 1 }
@@ -60,4 +61,6 @@ level1=psi(psiInv(u[9],theta1)+psiInv(u[2],theta1)+psiInv(u[7],theta1)+psiInv(u[
 level0=psi(psiInv(u[3],theta0)+psiInv(u[6],theta0)+psiInv(u[1],theta0)+psiInv(level1,theta0),theta0)
 stopifnot(all.equal(v,level0,tol = 1e-14))                  
 
+## test rn()
+rn(c9,100)
 
