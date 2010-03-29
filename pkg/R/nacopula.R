7967b47395fee22032c01c5494de8c00ca9ba0be
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-29 11:14:46 +0000

    implemented "..." feature
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/nacopula.R
+++ pkg/R/nacopula.R
@@ -19,35 +19,35 @@ setMethod("value", signature(x ="nACopula"),
     })
 
 ### returns U : matrix(*,n,d)
-setGeneric("rn", function(x,n) standardGeneric("rn"))
+setGeneric("rn", function(x,n,...) standardGeneric("rn"))
 
 setMethod("rn", signature(x ="final_nACopula"),
-    function(x,n) {
+    function(x,n,...) {
 	      Cout <- x@copula#outer copula
 	      theta <- Cout@theta#theta for outer copula
 	      V0 <- Cout@V0(n,theta)#generate V0's
-	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cout@psiInv,theta0=theta,V0=V0)#start recursion
+	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cout@psiInv,theta0=theta,V0=V0,...)#start recursion
 	      dns <- length(x@comp)#dimension of the non-sectorial part
         r <- matrix(runif(n*dns),nrow=n,ncol=dns)#generate the non-sectorial part
-        mat <- cbind(r, do.call(cbind,childdat$U))#put pieces together
+        mat <- cbind(r, do.call(cbind,lapply(childdat, `[[`, "U")))#put pieces together
         dat <- Cout@psi(-log(mat)/V0,theta=theta)#transform
         j <- c(x@comp,childdat$indCol)#get correct sorting order
         Cout@psi(dat[,j],theta)#permute data and return
     })
 
 ### returns list(U = matrix(*,n,d), indCol = vector of length d)
-setGeneric("rnchild", function(x,n,psi0Inv,theta0,V0) standardGeneric("rnchild"))
+setGeneric("rnchild", function(x,n,psi0Inv,theta0,V0,...) standardGeneric("rnchild"))
 
 ### for all inner copulas
 setMethod("rnchild", signature(x ="nACopula"),
-    function(x,n,psi0Inv,theta0,V0) {
+    function(x,n,psi0Inv,theta0,V0,...) {
         Cin <- x@copula#inner copula
 	      theta1 <- Cin@theta#theta_1 for inner copula
-	      V01 <- Cin@V01(V0,theta0,theta1)#generate V01's
-	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cin@psiInv,theta0=theta1,V0=V01)#recursion
+	      V01 <- Cin@V01(V0,theta0,theta1,...)#generate V01's
+	      childdat <- lapply(x@childCops,rnchild,n=n,psi0Inv=Cin@psiInv,theta0=theta1,V0=V01,...)#recursion
 	      dns <- length(x@comp)#dimension of the non-sectorial part
         r <- matrix(runif(n*dns),nrow=n,ncol=dns)#generate the non-sectorial part
-        mat <- cbind(r, do.call(cbind, childdat$U))#put pieces together
+        mat <- cbind(r, do.call(cbind,lapply(childdat, `[[`, "U")))#put pieces together
         dat <- exp(-V0*psi0Inv(C@psi(-log(mat),theta1),theta0))#transform
         j <- c(x@comp,childdat$indCol)#get correct sorting order
         list(U = dat, indCol = j)#get list and return
