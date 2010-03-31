8a0e1f56881aec492f32aa6ca4155c94d9890942
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-31 00:43:38 +0000

    included a 2d Clayton copula => rn() works! included a 3d example of a different family (AMH) => again, random data does not seem to be correct => Guess that this is due to rn() recursion
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@57 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/NAC-experi.R
+++ pkg/tests/NAC-experi.R
@@ -1,6 +1,70 @@
 library(nacopula)
 
-## 3d Clayton copula example
+#====2d Clayton copula example========================================
+c2 <-
+    new("outer_nACopula", copula = setTheta(copClayton, 0.5),
+        comp = as.integer(c(1,2)) # no childCops
+        )
+        
+## basic check
+d <- dim(c2)
+stopifnot(d == 2,
+	  allComp(c2) == 1:2)
+
+## test value()
+(v <- value(c2, u = c(.3, .4)))
+stopifnot(all.equal(v,
+ local({ u1 <- .3; u2 <- .4
+        (u1^(-1/2)+u2^(-1/2)-1)^(-2)}),
+                    tol = 1e-14))
+
+## test rn()
+nn <- 2000
+rC2 <- rn(c2,nn)
+stopifnot(is.numeric(rC2), is.matrix(rC2),
+	  dim(rC2) == c(nn, 2))
+C2 <- cor(rC2,method="kendall")
+formatC(round(C2, 3),format="f",digits=3)#theoretical value: 0.2
+pairs(rC2, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+
+#====3d Ali-Mikhail-Haq copula example========================================
+c3 <-
+    new("outer_nACopula", copula = setTheta(copAMH, 0.7135),
+        comp = as.integer( 1 ),
+        childCops = list(new("nACopula",
+                             copula = setTheta(copAMH, 0.9430),
+                             comp = as.integer(c(2,3)))) # no childCops
+        )
+
+## basic check
+d <- dim(c3)
+stopifnot(d == 3,
+	  allComp(c3) == 1:3,
+	  allComp(c3@childCops[[1]]) == 2:3)
+
+## test value()
+u <- c(.3, .4, .5)
+## with value function:
+(v <- value(c3, u))
+## by hand
+psi <- function(t,theta) { (1-theta)/(exp(t)-theta) }
+psiInv <- function(t,theta) { log((1-theta*(1-t))/t) }
+th0 <- 0.7135
+th1 <- 0.9430
+level1 <- psi(psiInv(u[2],th1) + psiInv(u[3],th1), th1)
+level0 <- psi(psiInv(u[1],th0) + psiInv(level1, th0), th0)
+stopifnot(all.equal(v, level0, tol = 1e-14))
+
+## test rn()
+nn <- 2000
+rC3 <- rn(c3,nn)
+stopifnot(is.numeric(rC3), is.matrix(rC3),
+	  dim(rC3) == c(nn, 3))
+C3 <- cor(rC3,method="kendall")
+formatC(round(C3, 3),format="f",digits=3)#theoretical values: C3_{12}=0.2, C3_{13}=0.2, C3_{23}=0.3
+pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+
+#====3d Clayton copula example========================================
 c3 <-
     new("outer_nACopula", copula = setTheta(copClayton, 0.5),
         comp = as.integer( 1 ),
@@ -16,7 +80,7 @@ stopifnot(d == 3,
 	  allComp(c3@childCops[[1]]) == 2:3)
 
 ## test value()
-(v <- value(c3, u = c(.3, .4, .5))) # 0.1514384
+(v <- value(c3, u = c(.3, .4, .5)))
 stopifnot(all.equal(v,
  local({ u1 <- .3; u2 <- .4; u3 <- .5
          1/((1/u2^2 +1/u3^2 -1)^(1/4) -1 +1/sqrt(u1))^2}),
@@ -28,10 +92,10 @@ rC3 <- rn(c3,nn)
 stopifnot(is.numeric(rC3), is.matrix(rC3),
 	  dim(rC3) == c(nn, 3))
 C3 <- cor(rC3,method="kendall")
-round(C3, 3)#theoretical values: C3_{12}=0.2, C3_{13}=0.2, C3_{23}=0.5
+formatC(round(C3, 3),format="f",digits=3)#theoretical values: C3_{12}=0.2, C3_{13}=0.2, C3_{23}=0.5
 pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
 
-## 9d Clayton copula example
+#====9d Clayton copula example========================================
 c9 <- new("outer_nACopula", copula = setTheta(copClayton, 0.5),
          comp = as.integer(c(3,6,1)),
          childCops = list(new("nACopula",
@@ -56,7 +120,7 @@ stopifnot(d == 9,
 ## test value()
 u <- seq(0.1,0.9,by=0.1)
 ## with value function:
-(v <- value(c9, u))# 0.0306986
+(v <- value(c9, u))
 ## by hand
 psi <- function(t,theta) { (1+t)^(-1/theta) }
 psiInv <- function(t,theta) { t^(-theta) - 1 }
@@ -79,7 +143,7 @@ stopifnot(all.equal(v, level0, tol = 1e-14))
 rC9 <- rn(c9,100)
 stopifnot(dim(rC9) == c(100, 9))
 C9 <- cor(rC9,method="kendall")
-round(C9, 3)
+formatC(round(C9, 3),format="f",digits=3)
 #theoretical values: 
 #C9_{12}=0.2, C9_{13}=0.2, C9_{14}=0.2, C9_{15}=0.2, C9_{16}=0.2, C9_{17}=0.2, C9_{18}=0.2, C9_{19}=0.2
 #             C9_{23}=0.2, C9_{24}=0.5, C9_{25}=0.5, C9_{26}=0.2, C9_{27}=0.5, C9_{28}=0.5, C9_{29}=0.5
