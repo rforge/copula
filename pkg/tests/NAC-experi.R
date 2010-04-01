116cf64a63cc5c2e50495edbb9343607953cae77
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-04-01 12:12:49 +0000

    Cleaned up our examples in NAC-experi.R as discussed. Included a large-dimensional example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/NAC-experi.R
+++ pkg/tests/NAC-experi.R
@@ -1,64 +1,72 @@
 library(nacopula)
+set.seed(1) 
+n <- 2000
+taueps <- 0.04
 
 #====3d examples: generate data by hand to check if V0 and V01 are correct========================================
 
-#correlation check function
-corcheck=function(n,th0,th1,cop){
+#correlation check function (copula sampled "by hand")
+corcheck<-function(n,th0,th1,cop){
   mat <- matrix(0,nrow=n,ncol=3)
-  cat("Run time V0:\n")
-  print(system.time(V0 <- cop@V0(n,th0)))
-  cat("\n")
+  V0time<-system.time(V0 <- cop@V0(n,th0))
   mat[,1] <- runif(n)
-  cat("Run time V01:\n")
-  print(system.time(V01 <- cop@V01(V0,th0,th1)))
-  cat("\n")
+  V01time<-system.time(V01 <- cop@V01(V0,th0,th1))
   mat[,2] <- exp(-V0*cop@psiInv(cop@psi(rexp(n)/V01,th1),th0))
   mat[,3] <- exp(-V0*cop@psiInv(cop@psi(rexp(n)/V01,th1),th0))
   mat[,] <- cop@psi(-log(mat[,])/V0,th0)
-  cat("Estimated matrix of pairwise Kendall's tau:\n")
-  print(formatC(round(cor(mat,method="kendall"), 3),format="f",digits=3),quote=F)
+  cormat<-cor(mat,method="kendall")
+  list(V0time,V01time,cormat)
 }
 
-nn <- 5000
+#create output
+corcheckout<-function(corcheck,family,mat){
+  cat("Run time V0 for ",family,":\n",sep="")
+  print(corcheck[[1]])
+  cat("\nRun time V01 for ",family,":\n",sep="")
+  print(corcheck[[2]])
+  cat("\nLargest distance between pairwise Kendall's taus:\n",sep="")
+  print(max(abs(corcheck[[3]]-mat)))
+}
 
 #AMH
-corcheck(nn,0.7135,0.9430,copAMH)#tau_{12}=tau_{13}=0.2, tau_{23}=0.3
-#C
-corcheck(nn,0.5,2,copClayton)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
-#F
-corcheck(nn,1.8609,5.7363,copFrank)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
-#G
-corcheck(nn,1.25,2,copGumbel)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
-#J
-corcheck(nn,1.4438,2.8562,copJoe)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+corcheckAMH<-corcheck(n,0.7135,0.9430,copAMH)
+mat<-matrix(c(1,0.2,0.2,0.2,1,0.3,0.2,0.3,1),nrow=3,byrow=TRUE)#tau_{12}=tau_{13}=0.2, tau_{23}=0.3
+corcheckout(corcheckAMH,"AMH",mat)
+stopifnot(max(abs(corcheckAMH[[3]]-mat))<taueps)
 
-#====2d Clayton copula example========================================
-c2 <-
-    new("outer_nACopula", copula = setTheta(copClayton, 0.5),
-        comp = as.integer(c(1,2)) # no childCops
-        )
-        
-## basic check
-d <- dim(c2)
-stopifnot(d == 2,
-	  allComp(c2) == 1:2)
+#Clayton
+corcheckClayton<-corcheck(n,0.5,2,copClayton)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+mat<-matrix(c(1,0.2,0.2,0.2,1,0.5,0.2,0.5,1),nrow=3,byrow=TRUE)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+corcheckout(corcheckClayton,"Clayton",mat)
+stopifnot(max(abs(corcheckClayton[[3]]-mat))<taueps)
 
-## test value()
-(v <- value(c2, u = c(.3, .4)))
-stopifnot(all.equal(v,
- local({ u1 <- .3; u2 <- .4
-        (u1^(-1/2)+u2^(-1/2)-1)^(-2)}),
-                    tol = 1e-14))
+#Frank
+corcheckFrank<-corcheck(n,1.8609,5.7363,copFrank)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+corcheckout(corcheckFrank,"Frank",mat)
+stopifnot(max(abs(corcheckFrank[[3]]-mat))<taueps)
 
-## test rn()
-rC2 <- rn(c2,nn)
-stopifnot(is.numeric(rC2), is.matrix(rC2),
-	  dim(rC2) == c(nn, 2))
-C2 <- cor(rC2,method="kendall")
-formatC(round(C2, 3),format="f",digits=3)#theoretical value: 0.2
-pairs(rC2, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+#Gumbel
+corcheckGumbel<-corcheck(n,1.25,2,copGumbel)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+corcheckout(corcheckGumbel,"Gumbel",mat)
+stopifnot(max(abs(corcheckGumbel[[3]]-mat))<taueps)
+
+#Joe
+corcheckJoe<-corcheck(n,1.4438,2.8562,copJoe)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+corcheckout(corcheckJoe,"Joe",mat)
+stopifnot(max(abs(corcheckJoe[[3]]-mat))<taueps)
+
+#====Examples that check value() and rn()========================================
+
+#generate output for the examples
+output=function(cormat,mat,rt){
+  cat("Run time for generating ",n," vectors of variates:\n",sep="")
+  print(rt)
+  cat("\nLargest distance between pairwise Kendall's taus:\n",sep="")
+  print(max(abs(cormat-mat)))
+}
 
 #====3d Ali-Mikhail-Haq copula example========================================
+
 c3 <-
     new("outer_nACopula", copula = setTheta(copAMH, 0.7135),
         comp = as.integer( 1 ),
@@ -76,7 +84,7 @@ stopifnot(d == 3,
 ## test value()
 u <- c(.3, .4, .5)
 ## with value function:
-(v <- value(c3, u))
+v <- value(c3, u)
 ## by hand
 psi <- function(t,theta) { (1-theta)/(exp(t)-theta) }
 psiInv <- function(t,theta) { log((1-theta*(1-t))/t) }
@@ -87,14 +95,44 @@ level0 <- psi(psiInv(u[1],th0) + psiInv(level1, th0), th0)
 stopifnot(all.equal(v, level0, tol = 1e-14))
 
 ## test rn()
-rC3 <- rn(c3,nn)
-stopifnot(is.numeric(rC3), is.matrix(rC3),
-	  dim(rC3) == c(nn, 3))
+rt<-system.time(rC3 <- rn(c3,n))
 C3 <- cor(rC3,method="kendall")
-formatC(round(C3, 3),format="f",digits=3)#theoretical values: C3_{12}=0.2, C3_{13}=0.2, C3_{23}=0.3
-pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+mat<-matrix(c(1,0.2,0.2,0.2,1,0.3,0.2,0.3,1),nrow=3,byrow=TRUE)#tau_{12}=tau_{13}=0.2, tau_{23}=0.3
+stopifnot(is.numeric(rC3), is.matrix(rC3),
+	  dim(rC3) == c(n, 3),max(abs(C3-mat))<taueps)
+output(C3,mat,rt)
+# pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+
+#====2d Clayton copula example========================================
+
+c2 <-
+    new("outer_nACopula", copula = setTheta(copClayton, 0.5),
+        comp = as.integer(c(1,2)) # no childCops
+        )
+        
+## basic check
+d <- dim(c2)
+stopifnot(d == 2,
+	  allComp(c2) == 1:2)
+
+## test value()
+v <- value(c2, u = c(.3, .4))
+stopifnot(all.equal(v,
+ local({ u1 <- .3; u2 <- .4
+        (u1^(-1/2)+u2^(-1/2)-1)^(-2)}),
+                    tol = 1e-14))
+
+## test rn()
+rt<-system.time(rC2 <- rn(c2,n))
+C2 <- cor(rC2,method="kendall")
+mat<-matrix(c(1,0.2,0.2,1),nrow=2,byrow=TRUE)#tau_{12}=0.2
+stopifnot(is.numeric(rC2), is.matrix(rC2),
+	  dim(rC2) == c(n, 2), max(abs(C2-mat))<taueps)
+output(C2,mat,rt)
+# pairs(rC2, panel = function(...) {par(new=TRUE); smoothScatter(...)})
 
 #====3d Clayton copula example========================================
+
 c3 <-
     new("outer_nACopula", copula = setTheta(copClayton, 0.5),
         comp = as.integer( 1 ),
@@ -110,21 +148,23 @@ stopifnot(d == 3,
 	  allComp(c3@childCops[[1]]) == 2:3)
 
 ## test value()
-(v <- value(c3, u = c(.3, .4, .5)))
+v <- value(c3, u = c(.3, .4, .5))
 stopifnot(all.equal(v,
  local({ u1 <- .3; u2 <- .4; u3 <- .5
          1/((1/u2^2 +1/u3^2 -1)^(1/4) -1 +1/sqrt(u1))^2}),
                     tol = 1e-14))
 
 ## test rn()
-rC3 <- rn(c3,nn)
-stopifnot(is.numeric(rC3), is.matrix(rC3),
-	  dim(rC3) == c(nn, 3))
+rt<-system.time(rC3 <- rn(c3,n))
 C3 <- cor(rC3,method="kendall")
-formatC(round(C3, 3),format="f",digits=3)#theoretical values: C3_{12}=0.2, C3_{13}=0.2, C3_{23}=0.5
-pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
+mat<-matrix(c(1,0.2,0.2,0.2,1,0.5,0.2,0.5,1),nrow=3,byrow=TRUE)#tau_{12}=tau_{13}=0.2, tau_{23}=0.5
+stopifnot(is.numeric(rC3), is.matrix(rC3),
+	  dim(rC3) == c(n, 3),max(abs(C3-mat))<taueps)
+output(C3,mat,rt)
+# pairs(rC3, panel = function(...) {par(new=TRUE); smoothScatter(...)})
 
 #====9d Clayton copula example========================================
+
 c9 <- new("outer_nACopula", copula = setTheta(copClayton, 0.5),
          comp = as.integer(c(3,6,1)),
          childCops = list(new("nACopula",
@@ -149,7 +189,7 @@ stopifnot(d == 9,
 ## test value()
 u <- seq(0.1,0.9,by=0.1)
 ## with value function:
-(v <- value(c9, u))
+v <- value(c9, u)
 ## by hand
 psi <- function(t,theta) { (1+t)^(-1/theta) }
 psiInv <- function(t,theta) { t^(-theta) - 1 }
@@ -169,10 +209,8 @@ level0 <- psi(psiInv(u[3],th0)+
 stopifnot(all.equal(v, level0, tol = 1e-14))
 
 ## test rn()
-rC9 <- rn(c9,nn)
-stopifnot(dim(rC9) == c(nn, 9))
+rt<-system.time(rC9 <- rn(c9,n))
 C9 <- cor(rC9,method="kendall")
-cormat <- matrix(as.numeric(formatC(round(C9, 3),format="f",digits=3)),nrow=9,byrow=TRUE)
 r1 <- c(1.0,rep(0.2,8))
 r2 <- c(0.2,1.0,0.2,0.5,0.5,0.2,rep(0.5,3))
 r3 <- c(0.2,0.2,1.0,rep(0.2,6))
@@ -183,8 +221,6 @@ r7 <- c(0.2,0.5,0.2,0.5,0.5,0.2,1.0,0.5,0.5)
 r8 <- c(0.2,0.5,0.2,0.6,0.5,0.2,0.5,1.0,0.5)
 r9 <- c(0.2,0.5,0.2,0.5,0.5,0.2,0.5,0.5,1.0)
 mat <- matrix(c(r1,r2,r3,r4,r5,r6,r7,r8,r9),nrow=9,byrow=TRUE)
-cormat
-max(abs(cormat-mat))
 #theoretical values: 
 #C9_{11}=1.0, C9_{12}=0.2, C9_{13}=0.2, C9_{14}=0.2, C9_{15}=0.2, C9_{16}=0.2, C9_{17}=0.2, C9_{18}=0.2, C9_{19}=0.2
 #             C9_{22}=1.0, C9_{23}=0.2, C9_{24}=0.5, C9_{25}=0.5, C9_{26}=0.2, C9_{27}=0.5, C9_{28}=0.5, C9_{29}=0.5
@@ -195,5 +231,55 @@ max(abs(cormat-mat))
 #                                                                              C9_{77}=1.0, C9_{78}=0.5, C9_{79}=0.5
 #                                                                                           C9_{88}=1.0, C9_{89}=0.5
 #                                                                                                        C9_{99}=1.0
-pairs(rC9, gap = .01,
-      main = paste(nn," vectors of a ",d,"-dimensional nested Clayton copula",sep=""))
+stopifnot(dim(rC9) == c(n, 9),
+          max(abs(C9-mat))<taueps)
+output(C9,mat,rt)
+# pairs(rC9, gap = .01,
+# main = paste(n," vectors of a ",d,"-dimensional nested Clayton copula",sep=""))
+
+#====Examples that check rn() according to run time========================================
+
+#====125d Clayton copula example========================================
+
+c125 <-
+    new("outer_nACopula", copula = setTheta(copClayton, 0.5),
+        comp = as.integer(),
+        childCops = list(new("nACopula",
+                             copula = setTheta(copClayton, 2),
+                             comp = as.integer(1:10)),
+                         new("nACopula",
+                             copula = setTheta(copClayton, 3),
+                             comp = as.integer(11:40)), 
+                         new("nACopula",
+                              copula = setTheta(copClayton, 2),
+                              comp = as.integer(41:60)),  
+                         new("nACopula",
+                              copula = setTheta(copClayton, 2),
+                              comp = as.integer(61:85)), 
+                         new("nACopula",
+                              copula = setTheta(copClayton, 3),
+                              comp = as.integer(86:105)),   
+                         new("nACopula",
+                              copula = setTheta(copClayton, 2),
+                              comp = as.integer(106:125))    
+                         ) # no childCops
+        )
+        
+## basic check
+d <- dim(c125)
+stopifnot(d == 125,
+	  allComp(c125) == 1:125,
+	  allComp(c125@childCops[[1]]) == 1:10,
+	  allComp(c125@childCops[[2]]) == 11:40,
+	  allComp(c125@childCops[[3]]) == 41:60,
+	  allComp(c125@childCops[[4]]) == 61:85,
+	  allComp(c125@childCops[[5]]) == 86:105,
+	  allComp(c125@childCops[[6]]) == 106:125
+	  )
+
+## test rn()
+rt<-system.time(rC125 <- rn(c125,n))
+stopifnot(is.numeric(rC125), is.matrix(rC125),
+	  dim(rC125) == c(n, 125))
+cat("Run time for generating ",n," vectors of variates:\n",sep="")
+rt
