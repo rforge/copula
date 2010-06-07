db2cd3b50bad6acd22450f0112cbf6797673fb2d
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-07 15:08:45 +0000

    safeUroot() - extended, now with help-page; also update rFJoe.. page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@159 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/safeUroot.R
+++ pkg/R/safeUroot.R
@@ -2,7 +2,7 @@
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
-## Foundation; either version 3 of the License, or (at your option) any later 
+## Foundation; either version 3 of the License, or (at your option) any later
 ## version.
 ##
 ## This program is distributed in the hope that it will be useful, but WITHOUT
@@ -16,44 +16,59 @@
 ##' Find a root
 ##' @param f function
 ##' @param interval interval
+##' @param ...
 ##' @param lower lower endpoint
 ##' @param upper upper endpoint
 ##' @param f.lower function value at lower endpoint
 ##' @param f.upper function value at upper endpoint
-##' @param Sig sign of f.upper
+##' @param Sig *desired* sign of f(upper), or NULL
 ##' @param tol tolerance
 ##' @param maxiter maximal number of iterations
 ##' @param trace number determining tracing
 ##' @author Martin Maechler (from Martin's package "nor1mix")
-safeUroot <- function(f, interval, lower = min(interval), upper = max(interval),
-		      f.lower = f(lower), f.upper = f(upper),
-		      Sig = sign(f.upper),
-		      tol = .Machine$double.eps^0.25, maxiter = 1000, trace = 0,
-                      ...)
+safeUroot <-
+    function(f, interval, ...,
+	     lower = min(interval), upper = max(interval),
+	     f.lower = f(lower), f.upper = f(upper), Sig = NULL,
+	     tol = .Machine$double.eps^0.25, maxiter = 1000, trace = 0)
 {
-    if(trace >= 2)
-	cat(sprintf("search in [%g,%g]\n", lower, upper))
-
-    ## make sure we have Sig*f(lower) < 0 and Sig*f(upper) > 0:
-    delta.r <- 0.01*max(1e-7, abs(lower))
-    f.lo <- f.lower
-    while(Sig*f.lo > 0) {
-	f.lo <- f(lower <- lower - delta.r)
+    if(	  is.null(Sig) && f.lower * f.upper > 0 ||
+       is.numeric(Sig) && (Sig*f.lower > 0 || Sig*f.upper < 0)) {
 	if(trace)
-	    cat(sprintf(" .. modified lower: %g\n",lower))
-	delta.r <- 2 * delta.r
+	    cat(sprintf("search in [%g,%g]%s", lower, upper,
+			if(trace >= 2)"\n" else " ... "))
+	Delta <- function(u) 0.01* pmax(1e-7, abs(u))
+	## Two cases:
+	if(is.null(Sig)) {
+	    ## case 1)	'Sig' unspecified --> extend (lower, upper) at the same time
+	    delta <- Delta(c(lower,upper))
+	    while(f.lower*f.upper > 0 && any(iF <- is.finite(c(lower,upper)))) {
+		if(iF[1]) f.lower <- f(lower <- lower - delta[1])
+		if(iF[2]) f.upper <- f(upper <- upper + delta[2])
+		if(trace >= 2)
+		    cat(sprintf(" .. modified lower,upper: (%15g,%15g)\n",
+				lower,upper))
+		delta <- 2 * delta
+	    }
+	} else {
+	    ## case 2) 'Sig' specified --> typically change only *one* of lower, upper
+	    ## make sure we have Sig*f(lower) < 0 and Sig*f(upper) > 0:
+	    delta <- Delta(lower)
+	    while(Sig*f.lower > 0) {
+		f.lower <- f(lower <- lower - delta)
+		if(trace) cat(sprintf(" .. modified lower: %g\n",lower))
+		delta <- 2 * delta
+	    }
+	    delta <- Delta(upper)
+	    while(Sig*f.upper < 0) {
+		f.upper <- f(upper <- upper + delta)
+		if(trace) cat(sprintf(" .. modified upper: %g\n",upper))
+		delta <- 2 * delta
+	    }
+	}
+	if(trace && trace < 2)
+	    cat(sprintf("extended to [%g,%g]\n", lower, upper))
     }
-    delta.r <- 0.01*max(1e-7, abs(upper))
-    f.up <- f.upper
-    while(Sig*f.up < 0) {
-	f.up <- f(upper <- upper + delta.r)
-	if(trace)
-	    cat(sprintf(" .. modified upper: %g\n",upper))
-	delta.r <- 2 * delta.r
-    }
-
-    ## here, we require R >= 2.6.0 with the improved uniroot():
-    uniroot(f, lower=lower, upper=upper,
-	    f.lower = f.lo, f.upper = f.up,
-	    tol=tol, maxiter=maxiter, ...)
+    uniroot(f, ..., lower=lower, upper=upper, f.lower = f.lower, f.upper = f.upper,
+	    tol=tol, maxiter=maxiter)
 }
