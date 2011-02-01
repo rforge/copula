1772fe39a8230f060ec45374856697863ee2a03a
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-01 21:32:31 +0000

    GIG demo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@399 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/demo/GIG-demo.R
@@ -0,0 +1,76 @@
+library(nacopula) 
+library(Runuran)
+library(optimx)
+options(warn = 1) 
+
+#### ==== Demo for working with the two-parameter Archimedean GIG copula =======
+
+source(system.file("Rsource", "GIG.R", package="nacopula"))
+
+## ==== setup ==================================================================
+
+## set seed
+set.seed(1) 
+
+## ==== specify parameters ====
+
+d <- 5 # dimension
+tau <- 0.5 # specify Kendall's tau
+## specify theta_1 and theta_2 such that Kendall's tau equals the given tau
+theta1 <- 0.01
+theta <- c(theta1, tauInv.GIG(tau, theta1, interval=c(1e-12,100)))
+
+## initial interval bounds for optimization
+h <- c(0.05, 0.05) # for initial interval for theta_1 and theta_2
+I <- matrix(c(theta[1]-h[1], theta[1]+h[1], theta[2]-h[2], theta[2]+h[2]), ncol=2) # initial intervals for theta_1 and theta_2
+
+## ==== sample the copula ====
+
+n <- 1000
+U <- rnacopula.GIG(n, d, theta)
+tau.hat <- cor(U, method="kendall")
+tau.hats <- tau.hat[upper.tri(tau.hat)]
+stopifnot(all.equal(rep(tau, d*(d-1)/2), tau.hats, tol=0.03)) # check tau
+
+## plot the sample
+pairs(U, gap=0)
+
+## ==== estimate the parameters ====
+
+n <- 100 # choose small sample size due to run time [depending on psi^{-1}]
+U <- rnacopula.GIG(n, d, theta)
+
+## choose random initial point in a reasonable interval
+start <- c(runif(1, min=I[1,1], max=I[1,2]), runif(1, min=I[2,1], max=I[2,2]))
+
+## call optimizer for MLE
+system.time(res.MLE <- optimx(par=start, 
+                              fn=function(x) -sum(dacopula.GIG(U, x, n.MC=0, log=TRUE)), 
+                              lower=c(I[1,1],I[2,1]), upper=c(I[1,2],I[2,2]), 
+                              method="bobyqa"))
+res.MLE
+
+## call optimizer for SMLE
+system.time(res.SMLE <- optimx(par=start, 
+                               fn=function(x) -sum(dacopula.GIG(U, x, n.MC=10000, log=TRUE)), 
+                               lower=c(I[1,1],I[2,1]), upper=c(I[1,2],I[2,2]), 
+                               method="bobyqa"))
+res.SMLE
+
+## note: run time is mainly determined by the evaluations of psiInv.GIG
+
+## ==== plot Kendall's tau as a function in theta for different nu's ====
+
+theta1 <- c(0, 0.001, 0.5, 1, 5, 10)
+cols <- colorRampPalette(c("red", "orange", "darkgreen", "turquoise", "blue"), 
+                         space="Lab")(length(theta1))
+for(i in seq_along(theta1))
+    curve(tau.GIG.vec(theta1[i],x), 1e-12, 2, 
+          main="Kendall's tau for the GIG family", ylim=c(0,1),
+          xlab=expression(theta[2]), ylab=expression(tau(theta[1],theta[2])), add=(i>1), 
+          lwd=1.4, col=cols[i])
+label <- as.expression(lapply(1:length(theta1), function(i) substitute(theta[1]==th, list(th=theta1[i]))))
+legend("topright", label, bty="n", lwd=1.4, col=cols)
+## conclusion: - largest range of tau's for theta_1 = 0
+##             - not possible to evaluate for theta_1 < 0
+##             - tau.GIG is numerically critical for theta_1 > 0 close to 0             
--- pkg/inst/Rsource/GIG.R
+++ pkg/inst/Rsource/GIG.R
@@ -1,12 +1,12 @@
 ## ==== GIG generator and related functions ====================================
 
-## generator (parameters nu in IR, theta in (0,Inf))
-## note: nu!=0 and theta=0 reduces to copClayton@psi(t, 1/nu)
+## generator (parameters theta_1 in IR, theta_2 in (0,Inf))
+## note: theta_1!=0 and theta_2=0 reduces to copClayton@psi(t, 1/theta_1)
 psi.GIG <- function(t, theta)
     (1+t)^(-theta[1]/2)*besselK(theta[2]*sqrt(1+t),theta[1])/besselK(theta[2],theta[1])
 
 ## generator inverse 
-## note: the initial interval is only valid for nu > -1/2
+## note: the initial interval is only valid for theta_1 > -1/2
 ##       it can be large, but it's finite; it is based on an inequality about 
 ##       modified Bessel functions of the third kind (besselK) given in 
 ##       Paris (1984) ("An inequality for the Bessel function J_v(vx)", 
@@ -75,7 +75,7 @@ dacopula.GIG <- function(u, theta, n.MC=0, log=FALSE){
         ## result
         res[n01] <- h(psiI.sum, k=d, theta=theta, log=TRUE) + (d-1)*
             log(besselK(theta[2], nu=theta[1])) - rowSums(h(psiI, k=1, theta=theta, 
-                               log=TRUE))
+                                  log=TRUE))
     }
     if(log) res else exp(res)
 }
@@ -93,11 +93,10 @@ dV0.GIG <- function(x, theta, log=FALSE){
     if(log) log(ud(dens, x)) else ud(dens, x)
 }
 
-## Kendall's tau for fixed nu as a function in theta
+## Kendall's tau for fixed theta_1 as a function in theta_2
 ## note: this is numerically difficult, take, e.g., tau.GIG(1, 400)
 ##       or even tau.GIG(0.00001,0.00001)
-tau.GIG. <- function(theta1, theta2, method=c("direct", "transformation"), ...){
-    theta <- c(theta1, theta2)
+tau.GIG <- function(theta, method=c("direct", "transformation"), ...){
     method <- match.arg(method)
     switch(method,
            "direct" = {
@@ -112,15 +111,28 @@ tau.GIG. <- function(theta1, theta2, method=c("direct", "transformation"), ...){
                integrand <- function(t, theta)
                    (t^2-theta[2]^2)/t^(4*theta[1]+3)*(t^(theta[1]+1)*besselK(t, theta[1]+1, expon.scaled=TRUE)*exp(-t))^2
                1-2*theta[2]^(4*theta[1])*(theta[2]^theta[1]*besselK(theta[2], theta[1], expon.scaled=TRUE)*exp(-theta[2]))^(-2)*
-                   integrate(integrand, lower=theta, upper=Inf, theta=theta, ...)$value
+                   integrate(integrand, lower=theta[2], upper=Inf, theta=theta, ...)$value
            },
        {stop(sprintf("unsupported method '%s' in psiDabsMC", method))})  
 }
-tau.GIG <- Vectorize(tau.GIG., "theta2") # vectorize in theta
+
+## vectorized wrapper of tau.GIG in theta_2 for given theta_1 [can be a number or a vector]
+tau.GIG.vec <- function(theta1, theta2, method=c("direct", "transformation"), ...){
+    n.1 <- length(theta1)
+    n.2 <- length(theta2)
+    stopifnot(n.1==1 || n.1==n.2)
+    if(n.1==1) theta1 <- rep(theta1, n.2)
+    theta <- cbind(theta1, theta2)
+    apply(theta, 1, tau.GIG, method=method, ...)
+}
 
 ## tau inverse for fixed theta_1
 ## note: initial interval has to be specified [e.g., c(1e-12,100)] since there 
 ##       are no adequate bounds known
 tauInv.GIG <- function(tau, theta1, interval, ...)
-    uniroot(function(th) tau.GIG(theta1, th)-tau, interval=interval, ...)$root
+    uniroot(function(th) tau.GIG(c(theta1,th))-tau, interval=interval, ...)$root
+
+## generate vectors of random variates from a GIG copula
+rnacopula.GIG <- function(n, d, theta)
+    psi.GIG(matrix(rexp(n*d), nrow=n, ncol=d)/V0.GIG(n, theta), theta)
 
