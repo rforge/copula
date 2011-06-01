da5c2507d51c91e5ff35386643c13c5d8153d026
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-06-01 13:18:12 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@530 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/opC-demo.R
+++ pkg/demo/opC-demo.R
@@ -1,4 +1,4 @@
-## Copyright (C) 2010 Marius Hofert and Martin Maechler
+## Copyright (C) 2011 Marius Hofert and Martin Maechler
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -17,34 +17,30 @@
 
 ## ==== setup ==================================================================
 
-library(nacopula) 
+library(nacopula)
 library(bbmle)
-library(lattice) 
-library(grid) 
-
-## FIXME: the following two lines are ignored! Further, par() opens a quartz device...
-par(ask=TRUE)
-grid.prompt(TRUE) 
+library(lattice)
+library(grid)
 
 ## specify parameters
 n <- 100 # sample size
 d <- 10 # dimension
-thetabase <- 1 
+thetabase <- 1
 tau <- 0.5 # => psi(t) = psi_thetabase(t^(1/theta)) with (thetabase,theta) = (1,4/3) (see below)
 
-## adjustment for initial value 
+## adjustment for initial value
 h <- c(0.4,0) # h_-, h_+
 
 ## ==== functions ==============================================================
 
 ##' Initial interval for opC
-##' 
+##'
 ##' @title Initial interval for opC
 ##' @param U (n x d)-matrix of simulated data
 ##' @param h non-negative auxiliary parameter for computing initial intervals
 ##' @param method "etau" via sample version of Kendall's tau (may be slow)
 ##'               "dmle.G" via DMLE of Gumbel (may be inaccurate)
-##' @return (2 x 2)-matrix containing the initial interval [1st row: lower, 
+##' @return (2 x 2)-matrix containing the initial interval [1st row: lower,
 ##'         2nd row: upper; 2 parameters => 2 cols]
 ##' @author Marius Hofert
 ii.opC <- function(U, h, method=c("etau","dmle.G")){
@@ -59,7 +55,7 @@ ii.opC <- function(U, h, method=c("etau","dmle.G")){
                       },
                       "dmle.G" = { # uses DMLE for Gumbel to estimate tau
                           Z <- apply(U, 1, max)
-                          theta.hat.G <- log(ncol(U))/(log(length(Z))-log(sum(-log(Z)))) 
+                          theta.hat.G <- log(ncol(U))/(log(length(Z))-log(sum(-log(Z))))
                           copGumbel@tau(theta.hat.G)
                       },
                       stop("ii.opC: wrong method"))
@@ -67,7 +63,7 @@ ii.opC <- function(U, h, method=c("etau","dmle.G")){
     if(tau.hat<=0) stop("ii.opC: tau.hat <= 0")
     tau.hat.hp <- min(tau.hat+h[2], 0.995)
     th.max <- 2*tau.hat.hp/(1-tau.hat.hp)
-    I[2,1] <- th.max # largest value for theta (= thetabase) 
+    I[2,1] <- th.max # largest value for theta (= thetabase)
     I[1,2] <- 1 # smallest value for beta (= theta)
     ## compute smallest theta (for lower left endpoint of the inital interval)
     tau.hat.hm <- max(tau.hat-h[1], 0.005) # tau=0.005 <=> theta=0.01
@@ -81,10 +77,10 @@ ii.opC <- function(U, h, method=c("etau","dmle.G")){
 }
 
 ##' -log-likelihood
-##' 
+##'
 ##' @title -log-likelihood
 ##' @param thetabase parameter of the base (=Clayton) generator
-##' @param theta outer power parameter 
+##' @param theta outer power parameter
 ##' @param u (n x d)-matrix of simulated data
 ##' @return -sum(log(density))
 ##' @author Marius Hofert
@@ -109,37 +105,37 @@ cop <- onacopulaL(opC, list(theta, 1:d))
 
 ## sample
 set.seed(1000)
-U <- rnacopula(n, cop) 
+U <- rnacopula(n, cop)
 
 ## initial interval and value
 I <- ii.opC(U, h)
 start <- colMeans(I)
 
 ## without profiling: optim with "L-BFGS-B"
-optim(par=start, method="L-BFGS-B", 
-      fn=function(x) nlogl.opC(x[1], theta=x[2], u=U), 
+optim(par=start, method="L-BFGS-B",
+      fn=function(x) nlogl.opC(x[1], theta=x[2], u=U),
       lower=c(I[1,1], I[1,2]), upper=c(I[2,1], I[2,2]))
 
 ## with profiling: via mle (using optim with "L-BFGS-B")
 nLL <- function(thetabase, theta) nlogl.opC(thetabase, theta, u=U)
-ml <- mle(nLL, method="L-BFGS-B", 
-          start=list(thetabase=mean(I[,1]), theta=mean(I[,2])), 
-          lower=c(thetabase=I[1,1], theta=I[1,2]), 
+ml <- mle(nLL, method="L-BFGS-B",
+          start=list(thetabase=mean(I[,1]), theta=mean(I[,2])),
+          lower=c(thetabase=I[1,1], theta=I[1,2]),
           upper=c(thetabase=I[2,1], theta=I[2,2]))
 summary(ml)
 ml@details
 
 ## with profiling: via mle2 (which uses optim with "L-BFGS-B")
-ml2 <- mle2(nlogl.opC, data=list(u=U), method="L-BFGS-B", 
-            start=list(thetabase=mean(I[,1]), theta=mean(I[,2])), 
-            lower=c(thetabase=I[1,1], theta=I[1,2]), 
+ml2 <- mle2(nlogl.opC, data=list(u=U), method="L-BFGS-B",
+            start=list(thetabase=mean(I[,1]), theta=mean(I[,2])),
+            lower=c(thetabase=I[1,1], theta=I[1,2]),
             upper=c(thetabase=I[2,1], theta=I[2,2]))
 summary(ml2)
 ml2@details
 
 ## ==== plots ==================================================================
 
-## ==== profile likelihood plots =============================================== 
+## ==== profile likelihood plots ===============================================
 
 prof <- profile(ml)
 (ci <- confint(prof))
@@ -150,9 +146,9 @@ prof2 <- profile(ml2)
 (ci <- confint(prof2))
 plot(prof2, main="Profile plot for mle2()")
 
-## ==== -log-likelihood plots ================================================== 
+## ==== -log-likelihood plots ==================================================
 
-## for the plots, we use the standard mathematical notation (theta, beta) 
+## for the plots, we use the standard mathematical notation (theta, beta)
 ## instead of (thetabase, theta)
 
 ## build grid
@@ -160,7 +156,7 @@ m <- 20 # number of grid points = number of intervals + 1
 th <- seq(I[1,1], I[2,1], length.out=m) # grid points for thetabase
 beta <- seq(I[1,2], I[2,2], length.out=m) # grid points for theta
 grid <- expand.grid(theta=th, beta=beta) # grid
-val.grid <- apply(grid, 1, nlogl.opC., u=U) # value of the -log-likelihood on the grid 
+val.grid <- apply(grid, 1, nlogl.opC., u=U) # value of the -log-likelihood on the grid
 
 ## ==== wireframe ====
 
@@ -169,20 +165,20 @@ true.theta <- c(thetabase, theta)
 true.val <- c(true.theta, nlogl.opC.(true.theta, u=U)) # theoretical optimum
 opt <- ml@coef # optimizer-optimum
 opt.val <- c(opt, nlogl.opC.(opt, u=U)) # optimizer-optimum and its value
-pts <- rbind(true.val, opt.val) # points to add to wireframe plot 
+pts <- rbind(true.val, opt.val) # points to add to wireframe plot
 title <- "-log-likelihood of an outer power Clayton copula" # title
 sub <- substitute(expression(paste(italic(n)==n.,"      ",italic(d)==d.,"      ",
-    tau==tau.,"      #: ",num.iter,sep="")), list(n.=n, d.=d, tau.=tau, 
+    tau==tau.,"      #: ",num.iter,sep="")), list(n.=n, d.=d, tau.=tau,
     num.iter=ml@details$counts)) # subtitle
 
 ## wireframe
 wireframe(val.grid~grid[,1]*grid[,2], screen=list(z=70, x=-55), zoom=0.95,
           xlab=expression(italic(theta)), ylab=expression(italic(beta)),
-          zlab=list(expression(paste(-log~L,"(",theta,",",beta,")",sep="")), rot=90), 
+          zlab=list(expression(paste(-log~L,"(",theta,",",beta,")",sep="")), rot=90),
           main=title, sub=sub, pts=pts, scales=list(col=1, arrows=FALSE),
-          par.settings=list(axis.line=list(col="transparent"), 
-          clip=list(panel="off")), zlim=c(min(val.grid, pts[,3]), 
-                                   max(val.grid, pts[,3])), aspect=1, 
+          par.settings=list(axis.line=list(col="transparent"),
+          clip=list(panel="off")), zlim=c(min(val.grid, pts[,3]),
+                                   max(val.grid, pts[,3])), aspect=1,
           panel.3d.wireframe = function(x,y,z,xlim,ylim,zlim,xlim.scaled,
           ylim.scaled,zlim.scaled,pts,...){
               panel.3dwire(x=x, y=y, z=z, xlim=xlim, ylim=ylim, zlim=zlim,
@@ -191,10 +187,10 @@ wireframe(val.grid~grid[,1]*grid[,2], screen=list(z=70, x=-55), zoom=0.95,
               panel.3dscatter(x=pts[,1], y=pts[,2], z=pts[,3],
                               xlim=xlim, ylim=ylim, zlim=zlim,
                               xlim.scaled=xlim.scaled, ylim.scaled=ylim.scaled,
-                              zlim.scaled=zlim.scaled, type="p", col=c("red","blue"), 
+                              zlim.scaled=zlim.scaled, type="p", col=c("red","blue"),
                               pch=c(3,4), lex=2, cex=1.4, .scale=TRUE, ...)
           }, key=list(x=0.64, y=1.01, points=list(pch=c(3,4), col=c("red","blue"),
-                                      lwd=2, cex=1.4), 
+                                      lwd=2, cex=1.4),
              text=list(c("True value", "Optimum of optimizer")), padding.text=3,
              cex=1, align=TRUE, transparent=TRUE))
 
@@ -209,9 +205,9 @@ cols <- adjustcolor(colorRampPalette(c("darkgreen", "green", "orange", "yellow")
                                      space="Lab")(100), 0.8)
 
 ## levelplot
-levelplot(val.grid~grid[,1]*grid[,2], 
-          par.settings=list(layout.heights=list(main=3, sub=2), 
-          regions=list(col=cols)), 
+levelplot(val.grid~grid[,1]*grid[,2],
+          par.settings=list(layout.heights=list(main=3, sub=2),
+          regions=list(col=cols)),
           xlim=c(xlim.[1]-xeps, xlim.[2]+xeps),
           ylim=c(ylim.[1]-yeps, ylim.[2]+yeps),
           xlab=expression(italic(theta)), ylab=expression(italic(beta)),
@@ -219,11 +215,11 @@ levelplot(val.grid~grid[,1]*grid[,2],
           scales=list(alternating=c(1,1), tck=c(1,0)), contour=TRUE,
           panel=function(x, y, z, pts, ...){
               panel.levelplot(x=x, y=y, z=z, ...)
-              grid.points(x=pts[1,1], y=pts[1,2], pch=3, 
+              grid.points(x=pts[1,1], y=pts[1,2], pch=3,
                           gp=gpar(lwd=2, col="red")) # + true value
-              grid.points(x=pts[2,1], y=pts[2,2], pch=4, 
+              grid.points(x=pts[2,1], y=pts[2,2], pch=4,
                           gp=gpar(lwd=2, col="blue")) # x optimum
-          }, key=list(x=0.18, y=1.08, points=list(pch=c(3,4), col=c("red","blue"), 
+          }, key=list(x=0.18, y=1.08, points=list(pch=c(3,4), col=c("red","blue"),
                                       lwd=2, cex=1.4),
-             columns=2, text=list(c("True value", "Optimum of optimizer")), 
+             columns=2, text=list(c("True value", "Optimum of optimizer")),
              align=TRUE, transparent=TRUE))
