291bd2ff05409293046a141425a9cb7a992fac2c
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-25 15:42:10 +0000

    plot *BOTH* dDiag() slot  _and_  dDiagA()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@568 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/dDiag-plots.R
+++ pkg/demo/dDiag-plots.R
@@ -10,47 +10,61 @@ p.dDiag <-
     stopifnot(length(d) == 1, d == as.integer(d), d >= 2, n >= 10)
     cop <- getAcop(family)
     th. <- cop@tauInv(tau)
-    x <- seq(0,1, length = n)
-    yMat <- sapply(th., function(theta)
-                   dDiag(x, cop=onacopulaL(family, list(theta, 1:d)),
-                         log=log))
+    u <- seq(0,1, length = n)
     mainTit <-
         paste("Diagonal densities of ", family, "\n d = ",d,
               if(log)", log = TRUE", sep="")
-    non.fin <- !is.finite(yMat)
-    ## Rather: in log-scale, -Inf  may be ok :
-    if(log) non.fin <- non.fin & (is.na(yMat) | yMat != -Inf)
-    nF.mat <-
-        if(any(has.nF <- apply(non.fin, 2, any))) {
-            i.non.fin <- apply(non.fin, 2, which.max)
-            i.non.fin[!has.nF] <- NA
-            cat(mainTit,":\n non-finite values -- for (theta,  x >= *):\n")
-            print(cbind(theta = th.[has.nF], x = x[i.non.fin[has.nF]]))
-        }
-    matplot(x, yMat, type = "l", col = cols, lty = 1,
-            ylab="dDiag(x, *)", main= mainTit)
-    if(any(has.nF)) { ## mark the end points
-        i1 <- i.non.fin - 1
-        points(x[i1], yMat[cbind(i1, seq_along(tau))],
-               col = cols, cex = 1.5, lwd=2, pch = 4)
-    }
-    abline(h=0, lty=3)
     c.tau <- format(tau)
     c.th  <- format(th., scientific=FALSE, digits = 4)
-    lleg <- lapply(seq_along(tau), function(j) {
-        cc <- if(has.nF[j]) {
-            i <- i.non.fin[j]
-            sprintf("  f(%.3g) = %g", x[i], yMat[i,j])
-        }
-	substitute(list(tau == TAU, theta == TH) ~ COMM,
-                   list(TAU= c.tau[j], TH= c.th[j], COMM= cc))
 
-    })
-    legend(if(log)"bottomright" else "topleft",
-	   do.call(expression, lleg),
-	   lty = 1, col=cols, bty="n")
-    invisible(list(d = d, tau=tau, theta=th., x = x, y.dDiag = yMat,
-                   has.non.fin = has.nF, non.fin.mat = nF.mat))
+    ## yMat <- sapply(th., function(theta)
+    ##                dDiag(u, cop=onacopulaL(family, list(theta, 1:d)),
+    ##                      log=log))
+    ## The "generic" Archimedean  dDiag():
+    yM.a <- sapply(th., function(theta)
+                   nacopula:::dDiagA(u, d=d, cop= setTheta(cop, theta), log=log))
+    ## The dDiag-*s*lot :
+    yM.s <- sapply(th., function(theta)
+                   cop@dDiag(u, theta=theta, d=d, log=log))
+
+    p.oneMat <- function(yMat, label) {
+        thisTit <- paste(label, mainTit, sep=": ")
+        non.fin <- !is.finite(yMat)
+        ## Rather: in log-scale, -Inf  may be ok :
+        if(log) non.fin <- non.fin & (is.na(yMat) | yMat != -Inf)
+        nF.mat <-
+            if(any(has.nF <- apply(non.fin, 2, any))) {
+                i.non.fin <- apply(non.fin, 2, which.max)
+                i.non.fin[!has.nF] <- NA
+                cat(thisTit,":\n non-finite values -- for (theta,  u >= *):\n")
+                print(cbind(theta = th.[has.nF], u = u[i.non.fin[has.nF]]))
+            }
+        matplot(u, yMat, type = "l", col = cols, lty = 1,
+                ylab="dDiag(u, *)", main= thisTit)
+        if(any(has.nF)) { ## mark the end points
+            i1 <- i.non.fin - 1
+            points(u[i1], yMat[cbind(i1, seq_along(tau))],
+                   col = cols, cex = 1.5, lwd=2, pch = 4)
+        }
+        abline(h=0, lty=3)
+        lleg <- lapply(seq_along(tau), function(j) {
+            cc <- if(has.nF[j]) {
+                i <- i.non.fin[j]
+                sprintf("  f(%.3g) = %g", u[i], yMat[i,j])
+            }
+            substitute(list(tau == TAU, theta == TH) ~ COMM,
+                       list(TAU= c.tau[j], TH= c.th[j], COMM= cc))
+
+        })
+        legend(if(log)"bottomright" else "topleft",
+               do.call(expression, lleg),
+               lty = 1, col=cols, bty="n")
+    }## end{ p.oneMat() }
+
+    p.oneMat(yM.a, "dDiagA()")
+    p.oneMat(yM.s, "cop @ dDiag()")
+
+    invisible(list(d = d, tau=tau, theta=th., u = u, dDiag.a = yM.a, dDiag.s = yM.s))
 }
 
 par(ask = dev.interactive(orNone = TRUE))
