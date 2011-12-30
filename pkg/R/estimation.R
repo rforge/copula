1f1082f2afb19249eda77e6559f0df3b3843e2e6
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-30 13:15:29 +0000

    just added a comment: the problem of version 617 can be solved if tau.range is further truncated near one, but that should be done by hand if necessary
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@618 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/estimation.R
+++ pkg/R/estimation.R
@@ -38,7 +38,7 @@ initOpt <- function(family, tau.range=NULL, interval=TRUE, u, method=c("tau.Gumb
     cop <- getAcop(family)
     if(is.null(tau.range)){
         tau.range <- switch(cop@name, # limiting (attainable) taus that can be dealt with in estimation/optimization/root-finding
-                            "AMH" = { c(0, 1/3-5e-5) }, # FIXME: closer to 1, emle's mle2 fails
+                            "AMH" = { c(0, 1/3-5e-5) }, # FIXME: closer to 1, emle's mle2 fails; note: typically, Std. Error still not available and thus profile() may fail => adjust by hand
                             "Clayton" = { c(1e-8, 0.95) },
                             "Frank" = { c(1e-8, 0.94) }, # FIXME: beyond that, estimation.gof() fails for ebeta()!
                             "Gumbel" = { c(0, 0.95) },
