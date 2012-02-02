07c50ad73f9413856a30e799cb0d970281aecf9c
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-02 11:29:13 +0000

    rudimentary tests for retstable
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@636 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/retstable-ex.R
+++ pkg/tests/retstable-ex.R
@@ -1,4 +1,4 @@
-## Copyright (C) 2010 Marius Hofert and Martin Maechler
+## Copyright (C) 2010-2012  Marius Hofert and Martin Maechler
 ##
 ## This program is free software; you can redistribute it and/or modify it under
 ## the terms of the GNU General Public License as published by the Free Software
@@ -25,5 +25,33 @@ require(nacopula)
 source(system.file("Rsource", "utils.R", package="nacopula"))
 ##--> tryCatch.W.E(), canGet()
 
-###---- TODO --- *some* good "regression tests"
+## it works for 0-length  V0 as well:
+.N <- numeric(0) ; stopifnot(identical(.N, retstable(alpha, .N)))
+
+## This is from "next version of Matrix" test-tools-1.R:
+showSys.time <- function(expr) {
+    ## prepend 'Time' for R CMD Rdiff
+    st <- system.time(expr)
+    writeLines(paste("Time", capture.output(print(st))))
+    invisible(st)
+}
+
 ### using both retstableR() and retstable()
+set.seed(1)
+alpha <- .2
+V0 <- rgamma(2^12, 1)
+set.seed(17); showSys.time(rET   <- retstable (alpha, V0)) ## method = default: here takes
+							   ##	  983 times "MH",  17 x "LD"
+set.seed(17); showSys.time(rET.H <- retstable (alpha, V0, method= "MH"))
+set.seed(17); showSys.time(rET.D <- retstable (alpha, V0, method= "LD"))
+set.seed(17); showSys.time(rET.R <- retstableR(alpha, V0))
+T <- function(r) r^(1/8) # log() is too much
+bp <- boxplot(T(rET1), T(rET1.H), T(rET1.D), T(rET.R),
+              notch=TRUE, col = "thistle")
+(meds <- bp$stats[3,])
+
+## "H0":  The 4 groups are not different -- here for the medians:
+stopifnot(bp$conf[1,] < meds & meds < bp$conf[2,],
+          bp$stats > 0,
+          abs(bp$stats[2,] - 0.4035) < 0.007, ## first Quartiles
+          abs(bp$stats[4,] - 0.8085) < 0.006) ## 3rd   Quartiles
