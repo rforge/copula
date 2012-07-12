b67e44751a2334b2539f2abe87c6e5ce180e1f24
ikojadin <ikojadin@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-12 15:52:37 +0000

    Improved by Martin
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@746 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/demo/fitting-tests.R
+++ pkg/copula/demo/fitting-tests.R
@@ -9,29 +9,66 @@ do1 <- function(cop,n) {
   f.irho <- fitCopula(cop, u, method="irho")
   f.mpl <- fitCopula(cop, u, method="mpl")
   f.ml <- fitCopula(cop, x, method="ml")
-  c(itau.est = f.itau@estimate, irho.est = f.irho@estimate,
-    mpl.est  = f.mpl @estimate, ml.est   = f.ml  @estimate,
-    itau.se = sqrt(f.itau@var.est), irho.se = sqrt(f.irho@var.est),
-    mpl.se  = sqrt(f.mpl @var.est), ml.se   = sqrt(f.ml  @var.est))
+  cbind(est = c(itau = f.itau@estimate, irho = f.irho@estimate,
+          mpl = f.mpl@estimate, ml   = f.ml@estimate),
+        se = c(itau = sqrt(f.itau@var.est), irho = sqrt(f.irho@var.est),
+          mpl = sqrt(f.mpl@var.est), ml = sqrt(f.ml@var.est)))
 }
 
 ## N replicates with mean of estimates and se, and sd of estimates
 testCop <- function(cop, n, N) {
   sim <- replicate(N, do1 (cop,n))
-  m <- rowMeans(sim)
-  cbind(bias = m[1:4] - cop@parameters, ## bias
-        dSE  = m[5:8] - apply(sim[1:4,], 1, sd)) ## mean of se minus sd of estimates
+  m <- rowMeans(sim,dims=2)
+  cbind(bias = abs(m[,"est"] - cop@parameters), ## abs of bias
+        dSE  = abs(m[,"se"] - apply(sim[,1,],1,sd))) ## abs of mean of se minus sd of estimates
 }
 
-
-## test for one bivariate one-parameter copula family
-run1test <- function(cop, taus, n, N) {
-  theta <- structure(calibKendallsTau(cop,taus),
-		     names = paste("tau", format(taus), sep="="))
+##' Test of the methods in fitCopula for a bivariate one-parameter copula family
+##'
+##' @title Test of the methods in fitCopula for a bivariate one-parameter copula family
+##' @param cop is the copula family 
+##' @param tau.set is the set of tau values for which the parameters of cop are set
+##' @param n.set is the set of n values used in the simulation
+##' @param N is the number of repetitions for computing the bias and dSE
+##' @return an array bias and dSE in different tau and n scenarios
+##' @author Martin
+run1test <- function(cop, tau.set, n.set, N) {
+  theta <- structure(calibKendallsTau(cop,tau.set),
+		     names = paste("tau", format(tau.set), sep="="))
+  names(n.set) <- paste("n",format(n.set),sep="=")
   setPar <- function(cop, value) { cop@parameters <- value ; cop }
-  sapply(lapply(theta, setPar, cop=cop),
-	 testCop, n=n, N=N,
-	 simplify = "array")
+  cop.set <- lapply(theta, setPar, cop=cop)
+  sapply(cop.set,
+         function(cop)
+         sapply(n.set, function(n) testCop(cop,n,N), simplify = "array"),
+         simplify = "array")
 }
 
-rr <- run1test(normalCopula(dim=2), taus=seq(0.2,0.8,by=0.2), n=16, N= 8)
+
+##' Reshapes the results for processing by xyplot
+##' @title Reshapes the results for processing by xyplot
+##' @param res object returned by run1test
+##' @param taunum logical indicating whether the taus are numeric or character 
+##' @return a matrix containing the reshaped results
+##' @author Martin
+reshape.res <- function(res,taunum=FALSE) {
+  names(dimnames(res)) <- c("method","stat","n","tau")
+  d <- cbind(expand.grid(dimnames(res),KEEP.OUT.ATTRS=FALSE), x=as.vector(res))
+  d[,"n"  ] <- as.numeric(vapply(strsplit(as.character(d[, "n" ]),split="="),`[`,"",2))
+  if (taunum)
+    d[,"tau"] <- as.numeric(vapply(strsplit(as.character(d[,"tau"]),split="="),`[`,"",2))
+  d
+}
+
+## plot the results
+plots.res <- function(d, log=TRUE) {
+  require(lattice)
+  xyplot(x ~ n | stat*tau, groups=method, data=d,type="b",
+         scales = list(x=list(log=log), y=list(log=log)))
+}
+
+## test code
+rr <- run1test(normalCopula(), tau.set=seq(0.2,0.8,by=0.2), n.set=c(50,100,200), N=200)
+d <- reshape.res(rr)
+plots.res(d)
+plots.res(d, log=FALSE)
