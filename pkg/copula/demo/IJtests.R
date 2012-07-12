d92987eef308bc293196231732d3f3d111cd0476
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-12 07:03:18 +0000

    small changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@740 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/demo/IJtests.R
+++ pkg/copula/demo/IJtests.R
@@ -1,4 +1,4 @@
-## test of fitCopula 
+## test of fitCopula
 ######################################
 
 ## one replicate
@@ -9,28 +9,29 @@ do1 <- function(cop,n) {
   f.irho <- fitCopula(cop, u, method="irho")
   f.mpl <- fitCopula(cop, u, method="mpl")
   f.ml <- fitCopula(cop, x, method="ml")
-  c(itau.est = f.itau@estimate, irho.est = f.irho@estimate, mpl.est = f.mpl@estimate, ml.est = f.ml@estimate,
-    itau.se = sqrt(f.itau@var.est), irho.se = sqrt(f.irho@var.est), mpl.se = sqrt(f.mpl@var.est), ml.se = sqrt(f.ml@var.est))
+  c(itau.est = f.itau@estimate, irho.est = f.irho@estimate,
+    mpl.est  = f.mpl @estimate, ml.est   = f.ml  @estimate,
+    itau.se = sqrt(f.itau@var.est), irho.se = sqrt(f.irho@var.est),
+    mpl.se  = sqrt(f.mpl @var.est), ml.se   = sqrt(f.ml  @var.est))
 }
 
-## N replicates with mean of estimates and se, and sd of estimates 
+## N replicates with mean of estimates and se, and sd of estimates
 testCop <- function(cop, n, N) {
-  sim <- replicate(N,do1(cop,n))
+  sim <- replicate(N, do1 (cop,n))
   m <- rowMeans(sim)
-  b <- m[1:4] - cop@parameters ## bias
-  d <- m[5:8] - apply(sim[1:4,],1,sd) ## mean of se minus sd of estimates 
-  c(b,d)
+  cbind(bias = m[1:4] - cop@parameters, ## bias
+        dSE  = m[5:8] - apply(sim[1:4,], 1, sd)) ## mean of se minus sd of estimates
 }
-  
- 
+
+
 ## test for one bivariate one-parameter copula family
-run1test <- function(cop,taus=seq(0.2,0.8,by=0.2),n=100,N=10) {
-  res <- matrix(NA,length(taus),8)
-  theta <- calibKendallsTau(cop,taus)
-  for (i in 1:length(taus)) {
-    cop@parameters <- theta[i]
-    res[i,] <- testCop(cop, n, N)
-  }
-  cbind(taus,theta,res)
+run1test <- function(cop, taus, n, N) {
+  theta <- structure(calibKendallsTau(cop,taus),
+		     names = paste("tau", format(taus), sep="="))
+  setPar <- function(cop, value) { cop@parameters <- value ; cop }
+  sapply(lapply(theta, setPar, cop=cop),
+	 testCop, n=n, N=N,
+	 simplify = "array")
 }
- 
+
+rr <- run1test(normalCopula(dim=2), taus=seq(0.2,0.8,by=0.2), n=16, N= 8)
