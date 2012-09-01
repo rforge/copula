f519fca32ff3e7371ca2c2b453f22336ede42d44
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-01 13:04:41 +0000

    update simulation (and start on ada-7 = 24 core machine)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@849 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/inst/Rsource/gof-sim.R
+++ pkg/copula/inst/Rsource/gof-sim.R
@@ -13,16 +13,39 @@
 ## You should have received a copy of the GNU General Public License along with
 ## this program; if not, see <http://www.gnu.org/licenses/>.
 
+user <- Sys.getenv("USER")
+switch(user,
+       "mhofert"={ # account Marius Hofert (local)
+           rm(list=objects()) # remove previously loaded objects
+           setwd("~/R/MMMH/parallel/")
+       },
+       "maechler"={
+           setwd("~/R/MM/Pkg-ex/copula")
+           Mlibrary(copula)
+       },
+       stop("unsupported user"))
+
+options(warn=1) # print warnings as they occur
+
+## load packages
+stopifnot(require(copula))
+
+
 
 ### Simulation  a la  Genest et al (2009) -- of only one case 
 ### -------------------------------------
-gofC <- function(copula, H0copula, n, N, method, ...) {
-    gofCopula(copula, x = rCopula(n, H0copula), N=N, method=method, ...)$pvalue
+gofC <- function(copula, H0copula, n, N, method, seed=NULL,
+                 verbose=FALSE, ...) {
+    if(!is.null(seed)) set.seed(seed)
+    T <- system.time(
+        pV <- gofCopula(copula, x = rCopula(n, H0copula), N=N,
+                        method=method, verbose=verbose, ...)$pvalue)
+    c(pvalue = pV, T)# or rather list?
 }
 
 
 require("parallel")
-nC <- detectCores()
+(nC <- detectCores())
 
 ## As Genest et al.
 n <- 150
@@ -33,11 +56,14 @@ method <- "SnC"
 
 nRep <- 1000
 ## Testing:
-nRep <- 2
+nRep <- 20
 
-set.seed(17)
+## we set seed *INSIDE* set.seed(17)
 (thCl <- iTau(claytonCopula(), 0.5)) # 2
 rr <- mclapply(1:nRep, function(i)
-               gofC(normalCopula(dim=dim),
-                    H0copula = claytonCopula(thCl, dim=dim),
-                    n = n, N = N, method = method))
+	       gofC(normalCopula(dim=dim), n = n, N = N,
+		    H0copula = claytonCopula(thCl, dim=dim),
+		    seed = i, method = method),
+               mc.set.seed=FALSE,
+               mc.cores = detectCores())
+save(rr, n,dim,N,method,  file = "gof-sim_rr.rda")
