8699c3e751ecce38e2421cb0e39fa23791d53ff9
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-08 21:45:43 +0000

    update (several days ago; not tested again)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@852 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/inst/Rsource/gof-sim.R
+++ pkg/copula/inst/Rsource/gof-sim.R
@@ -30,13 +30,18 @@ options(warn=1) # print warnings as they occur
 ## load packages
 stopifnot(require(copula))
 
+stime <- function() paste(vapply(unclass(as.POSIXlt(Sys.time()))[3:1],
+                                 round, 0.12, digits=2), collapse=":")
+stime()
 
-
-### Simulation  a la  Genest et al (2009) -- of only one case 
+### Simulation  a la  Genest et al (2009) -- of only one case
 ### -------------------------------------
 gofC <- function(copula, H0copula, n, N, method, seed=NULL,
-                 verbose=interactive(), ...) {
+                 verbose=interactive(), heartbeat=0, fileHB, ...) {
     if(!is.null(seed)) set.seed(seed)
+    if(heartbeat) { ii <- if(!is.null(seed)) seed else heartbeat
+                    writeLines(sprintf("%s:%5d", stime(), ii), con = fileHB)
+                }
     T <- system.time(
         pV <- gofCopula(copula, x = rCopula(n, H0copula), N=N,
                         method=method, verbose=verbose, ...)$p.value)
@@ -53,19 +58,42 @@ dim <- 2
 N <- 1000
 tau <- 0.5
 
+## 1)
 method <- "SnC"
+H0copF <- claytonCopula
+  copF <- normalCopula
+## 2)
+method <- "SnB"
+H0copF <- tCopula
+  copF <- gumbelCopula
 
 H0copF <- claytonCopula
   copF <- normalCopula
-
 
 if(interactive()) {## Testing:
     N <- 16
     n <- 12
     nRep <- 3
 } else {
-    nRep <- 10000
-}##         ===== {MM: guess: ~ 30 hours, on 24 cores, ada-7 }
+    nRep <- 2000
+}##         ===== {MM: guess: ~ 6 hours, on 24 cores, ada-7 }
+## Memory growth : VIRT =  400M (RES =  220M) after  1h30
+##                 VIRT =  594M (RES =  313M) after  2h30
+##                 VIRT =  662M (RES =  481M) after  4h30
+##                 VIRT =  713M (RES =  532M) after  5h07
+##                 VIRT =  741M (RES =  560M) after  5h37
+##                 VIRT =  772M (RES =  591M) after  6h05
+##                 VIRT =  832M (RES =  651M) after  6h32
+##                 VIRT =  868M (RES =  687M) after  7h03
+##                 VIRT =  942M (RES =  761M) after  8h00
+##                 VIRT =  981M (RES =  800M) after  8h35
+##                 VIRT = 1024M (RES =  842M) after  9h11
+## --> Jobs ends in 10h 30 min {but with a batch error, because I edited this file}
+
+
+##  nRep <- 10000 ---> on 24 cores (ada-7, Aug.2012) starts accumulating memory
+##                ---> about 3 GB per node ..-> machine starts swapping!
+##                ---> not finished after > 40 hours ==> 'pkill -f exec/R'
 
 ## we set seed *INSIDE* set.seed(17)
 
@@ -75,19 +103,26 @@ Hcop <- H0copF(thet.0, dim=dim)
 
 stopifnot(require("parallel"))
 
-sFile <- paste0("gof-sim_", nmCop(cop), ":", nmCop(Hcop),
-                "_n=", n,
-                "_d=", dim,
-                "_tau=",formatC(tau),
-                "_nrep=",nRep, ".rda")
-sFile
+filHead <- paste0("gof-sim_", method, "_", nmCop(cop), ":", nmCop(Hcop),
+                  "_n=", n,
+                  "_d=", dim,
+                  "_tau=",formatC(tau),
+                  "_nrep=",nRep)
+(sFile <- paste0(filHead, ".rda"))
+fileHB <- file(paste0(filHead, ".heartbeat"))
+open(fileHB, open="wt")
+## in case it already exists, start re-writing at beginning:
+seek(fileHB, 0); truncate(fileHB)
 
-rr <- mclapply(1:nRep, function(i)
+
+rr <- lapply(1:nRep, function(i)
 	       gofC(cop, H0copula = Hcop, n = n, N = N,
-		    seed = i, method = method),
+		    seed = i, method = method,
+                    heartbeat=TRUE, fileHB=fileHB))
+
                mc.set.seed=FALSE,
                mc.cores = detectCores())
 
 save(rr, n,dim,N,method, cop, Hcop,
      file = sFile)
-
+close(fileHB)
