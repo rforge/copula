81585a51a35b254cd79d9617188d0b02b5d4c00d
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-24 01:22:21 +0000

    trying to get down to reasonable check time
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@895 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/tests/v34i09.R
+++ pkg/copula/tests/v34i09.R
@@ -6,6 +6,14 @@
 ####
 #### === http://www.jstatsoft.org/v34/i09/supp/2
 ##---------------------------------------------------------------------------
+## The following changes are made here: instead of   'N = 1000'  (5x)
+## we use   'N = N'  and set N here:
+N <- 1000 # == original simulation for paper; we use smaller N below !
+(doExtras <- copula:::doExtras())
+N <- if(doExtras) 100 else 12
+## [plus  one other (doExtras) clause below]
+##---------------------------------------------------------------------------
+
 
 ###################################################
 ### lossPrep
@@ -38,16 +46,17 @@ plot(pseudoLoss.ave, sub="(b) average rank for ties")
 ###################################################
 ### lossIndep
 ###################################################
-system.time(empsamp <- indepTestSim(nrow(pseudoLoss), p = 2, N = 1000, print.every = 0))
+system.time(empsamp <- indepTestSim(nrow(pseudoLoss), p = 2, N = N, print.every = 0))
+##_N_ 207 sec [nb-mm3]
 indepTest(pseudoLoss, empsamp)
 
 
 ###################################################
 ### lossGof
 ###################################################
-system.time(lossGof.gumbel.pb <- gofCopula(gumbelCopula(1), pseudoLoss, method="itau", simulation="pb", N = 1000, print.every = 0))
+system.time(lossGof.gumbel.pb <- gofCopula(gumbelCopula(1), pseudoLoss, method="itau", simulation="pb", N = N, print.every = 0))
 lossGof.gumbel.pb
-system.time(lossGof.gumbel.mult <- gofCopula(gumbelCopula(1), pseudoLoss, method="itau", simulation="mult", N = 1000))
+system.time(lossGof.gumbel.mult <- gofCopula(gumbelCopula(1), pseudoLoss, method="itau", simulation="mult", N = N))
 lossGof.gumbel.mult
 
 
@@ -72,14 +81,16 @@ myAnalysis <- function(myLoss) {
   fit.g <- fitCopula(gumbelCopula(1), pseudoLoss, method="itau")
   c(indep=indTest, gof.g=gof.g, gof.c=gof.c, gof.f=gof.f, gof.n=gof.n, gof.t=gof.t, gof.p=gof.p, est=fit.g@estimate, se=sqrt(fit.g@var.est))
 }
-myReps <- t(replicate(100, myAnalysis(myLoss)))
+if(doExtras) {
+#was myReps <- t(replicate(100, myAnalysis(myLoss)))
+myReps <- t(replicate(12, myAnalysis(myLoss)))
 round(apply(myReps, 2, summary),3)
-
+} else round(myAnalysis(myLoss), 3)
 
 ###################################################
 ### lossGof3
 ###################################################
-gofCopula(gumbelCopula(1), pseudoLoss.ave, method="itau", simulation="mult", N = 1000)
+gofCopula(gumbelCopula(1), pseudoLoss.ave, method="itau", simulation="mult", N = N)
 
 
 ###################################################
@@ -100,7 +111,8 @@ pseudoSR <- apply(rdj[,2:4], 2, rank)/(nrow(rdj) + 1)
 ### srMultSerialIndepTest
 ###################################################
 set.seed(123)
-system.time(srMultSerialIndepTest <- multSerialIndepTest(rdj[,2:4]^2, lag.max=4, print.every = 0))
+system.time(srMultSerialIndepTest <-
+            multSerialIndepTest(rdj[,2:4]^2, lag.max=4, N = N, print.every = 0))
 srMultSerialIndepTest
 dependogram(srMultSerialIndepTest)
 
@@ -108,7 +120,7 @@ dependogram(srMultSerialIndepTest)
 ###################################################
 ### multIndepTest
 ###################################################
-system.time(empsamp <- indepTestSim(nrow(pseudoSR), p = 3, N = 1000, print.every = 0))
+system.time(empsamp <- indepTestSim(nrow(pseudoSR), p = 3, N = N, print.every = 0))
 srMultIndepTest <- indepTest(pseudoSR, empsamp)
 srMultIndepTest
 dependogram(srMultIndepTest)
