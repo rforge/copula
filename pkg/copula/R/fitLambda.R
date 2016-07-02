e38e71b61206a1aa8b3816a7f71583801a1fdc7f
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-02 14:03:53 +0000

    new method for fitLambda
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@1401 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/R/fitLambda.R
+++ pkg/copula/R/fitLambda.R
@@ -17,61 +17,103 @@
 
 ##' @title Non-parametric Estimators of the Matrix of Tail Dependence Coefficients
 ##' @param u An (n, d)-data matrix of pseudo-observations
-##' @param p A probability (in (0,1)) used as 'cut-off' point
+##' @param method The method string
+##' @param p A (small) probability (in (0,1)) used as 'cut-off' point
 ##' @param lower.tail A logical indicating whether the lower or upper tail dependence
 ##'        coefficient is to be computed
 ##' @param verbose A logical indicating whether a progress bar is displayed
-##' @return Estimate of the (matrix of) tail dependence coefficients
-##'         (depending on p)
+##' @param ... Additional arguments passed on to optimize() if method = "t"
+##' @return Estimate of the matrix of tail dependence coefficients
 ##' @author Marius Hofert
-##' @note - See Jaworksi et al. (2009, p. 231) and Schmid and Schmidt (2007)
-##'         for the method implemented below. We use this only for d = 2
-##'       - If further methods are implemented, introduce 'method' argument
-##'         with name "Schmid.Schmidt" for the method below.
-fitLambda <- function(u, p, lower.tail = TRUE, verbose = FALSE)
+##' @note See Jaworksi et al. (2009, p. 231) and Schmid and Schmidt (2007)
+##'       for the method "Schmid.Schmidt". We use this formula only pairwise,
+##'       so for d = 2.
+fitLambda <- function(u, method = c("Schmid.Schmidt", "t"),
+                      p = ceiling(nrow(u)/10), # copula = tCopula(), => maybe for a general copula at some point...
+                      lower.tail = TRUE, verbose = FALSE, ...)
 {
     ## Checking
     if(!is.matrix(u)) u <- rbind(u, deparse.level=0L)
     d <- ncol(u)
-    stopifnot(0 <= u, u <= 1, 0 <= p, p <= 1,
-              length(p) == 1, d >= 2, is.logical(lower.tail))
+    stopifnot(0 <= u, u <= 1, 0 <= p, p <= 1, length(p) == 1,
+              d >= 2, is.logical(lower.tail), is.logical(verbose))
+    method <- match.arg(method)
+    if(verbose) { # setup progress bar
+        l <- 0 # counter
+        ch <- choose(d, 2) # number of pairs
+        pb <- txtProgressBar(max = ch, style = if(isatty(stdout())) 3 else 1) # setup progress bar
+        on.exit(close(pb)) # on exit, close progress bar
+    }
 
     ## Compute Lambda
-    Lam <- diag(1, nrow = d)
-    if(verbose) {
-        pb <- txtProgressBar(max = d, style = if(isatty(stdout())) 3 else 1)
-        on.exit(close(pb))
-    }
-    if(lower.tail) {
+    switch(method,
+    "Schmid.Schmidt" = {
+
+        ## Compute lambda for each pair
+        if(lower.tail) {
+            u. <- u
+        } else { # upper tail-dependence coefficient (compute lambda_L for survival copula)
+            u. <- 1-u
+        }
+        Lam <- diag(1, nrow = d)
         M <- matrix(pmax(0, p-u), ncol = d)
-        for(i in 1:(d-1)) {
-            for(j in (i+1):d)
+        for(i in 2:d) {
+            for(j in 1:(i-1))
                 Lam[i,j] <- mean(apply(M[, c(i, j)], 1, prod)) # \hat{Lambda}_{ij}
-            if(verbose) setTxtProgressBar(pb, i) # update progress bar
+            if(verbose) {
+                l <- l+1
+                setTxtProgressBar(pb, l) # update progress bar
+            }
         }
         int.over.Pi <- (p^2 / 2)^2
         int.over.M <- p^3 / 3
-    } else { # upper tail-dependence coefficient
-        M <- matrix(pmin(p, 1-u), ncol = d)
-        for(i in 1:(d-1)) {
-            for(j in (i+1):d)
-                Lam[i,j] <- mean(apply(M[, c(i, j)], 1, prod)) # \hat{Lambda}_{ij}
-            if(verbose) setTxtProgressBar(pb, i) # update progress bar
-        }
-        int.over.Pi <- (p*(2-p)/2)^2
-        int.over.M <- (1-(1-p)^2 * (1+2*p))/3
-    }
-    Lam <- (Lam - int.over.Pi) / (int.over.M - int.over.Pi) # proper scaling
-    if(d == 2) {
-        min(max(Lam[1,2], 0), 1)
-    } else {
+        Lam <- (Lam - int.over.Pi) / (int.over.M - int.over.Pi) # proper scaling
+
         ## Sanity adjustment
         Lam[Lam < 0] <- 0
         Lam[Lam > 1] <- 1
-        ## Symmetrize
-        Lam. <- Lam + t(Lam)
-        diag(Lam.) <- rep.int(1, d)
-        Lam.
-    }
+
+        ## Symmetrize and return
+        ii <- upper.tri(Lam)
+        Lam[ii] <- t(Lam)[ii]
+        Lam
+
+    },
+    "t" = {
+
+        ## -Log-likelihood for a t copula
+        nLLt <- function(nu, P, u) {
+            x <- qt(u, df = nu)
+            -sum(dmvt(x, sigma = P, df = nu, log = TRUE) - rowSums(dt(x, df = nu, log = TRUE)))
+        }
+
+        ## Containers
+        Lam <- diag(1, nrow = d) # matrix of pairwise estimated tail-dependence coefficients
+        P   <- diag(1, nrow = d) # matrix of pairwise estimated correlation coefficients
+        Nu  <- matrix(, nrow = d, ncol = d) # matrix of pairwise estimated degrees of freedom (NA on diagonal)
+
+        ## Compute lambda for each pair (manually here, for performance reasons)
+        for(i in 2:d) {
+            for(j in 1:(i-1)) { # go over lower triangular matrix
+                u. <- u[, c(i, j)]
+                P. <- sin(cor.fk(u.)*pi/2) # 2 x 2 matrix
+                rho <- P.[2,1]
+                P[i,j] <- rho
+                nu <- optimize(nLLt, interval = c(1e-4, 1e3), P = P., u = u., ...)$minimum
+                Nu[i,j] <- nu
+                Lam[i,j] <- 2 * pt(-sqrt((nu + 1) * (1 - rho) / (1 + rho)), df = nu + 1)
+                l <- l + 1
+                setTxtProgressBar(pb, l) # update progress bar
+            }
+        }
+        ## Symmetrize and return
+        ii <- upper.tri(Lam)
+        Lam[ii] <- t(Lam)[ii]
+        P[ii]   <- t(P)[ii]
+        Nu[ii]  <- t(Nu)[ii]
+        list(Lambda = Lam, P = P, Nu = Nu)
+
+    },
+    stop("Wrong 'method'"))
 }
 
