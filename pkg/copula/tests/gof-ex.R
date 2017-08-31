b2f00783811e12dcde03567deca9d648886c339a
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-08-31 13:41:31 +0000

    fix cCopula(*, invert=T) bug found by Marius Pfeuffer
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@1679 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/tests/gof-ex.R
+++ pkg/copula/tests/gof-ex.R
@@ -19,9 +19,17 @@ sessionInfo() # will change often.. but if we need the info, get it here
 
 ## Regression Tests -- n=1 failed till 2012-07-15
 u1 <- pobs(rbind(1:4))
-(rtG <- cCopula(u1, copula = onacopula("Gumbel", C(2., 1:4))))
-stopifnot(all.equal(rtG, cCopula(u1, copula = gumbelCopula(2, d=4)),
-		    tolerance = 1e-15))
+cG <- gumbelCopula(2, d=4)
+ocG<- onacopula("Gumbel", C(2., 1:4))
+(rtG  <- cCopula(u1, copula = ocG))
+(rtGI <- cCopula(u1, copula = ocG, inverse=TRUE))
+## failed with  " object 'n' not found " in 0.999.17
+stopifnot(TRUE
+    , all.equal(rtG,  cCopula(u1, copula = cG), tolerance = 1e-15)
+    , all.equal(rtG,  rbind(c(0.5, 0.530633, 0.4852854, 0.4567624)), tol = 7e-7)# seen 6.97e-8
+    , all.equal(rtGI, cCopula(u1, copula = cG, inverse=TRUE), tol = 1e-15)
+    , all.equal(rtGI, rbind(c(0.5, 0.4797781, 0.4997766, 0.5174252)),tol = 5e-7)# seen 4.97e-8
+)
 
 ### A faster, more checking version of demo(estimation.gof)
 ### that is, of ../demo/estimation.gof.R
