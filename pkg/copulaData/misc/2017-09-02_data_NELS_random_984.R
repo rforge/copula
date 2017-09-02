54224619a87214ca4cf93f63d076b7753f0124f9
mhofert <mhofert@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-09-02 18:44:41 +0000

    additional check
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@1686 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copulaData/misc/2017-09-02_data_NELS_random_984.R
+++ pkg/copulaData/misc/2017-09-02_data_NELS_random_984.R
@@ -39,6 +39,24 @@ urban.rural <- cbind(dat$Urban, dat$Rural) - 1 # note: converted to 1:2 here!
 table(rowSums(urban.rural))
 ## => At most one of 'urban' and 'rural' is 1, but they can both be 0
 
+## Check whether there are schools for which rural/urban differ
+## and whether there are schools for which the size differs
+num.uID <- table(dat$ID) # number of schools with a unique ID
+uID <- as.numeric(names(num.uID)) # IDs of schools with unique ID
+for(i in 1:length(uID)) {
+    if(num.uID[i] >= 2) {
+        dat. <- dat[dat$ID == uID[i],] # grab out all schools with that uID
+        if((length(unique(dat.$Urban)) != 1) ||  # check whether Rural and Urban differ
+           (length(unique(dat.$Rural)) != 1))
+            cat("Urban or Rural of school with ID ",uID[i]," differ!\n")
+        if(length(unique(dat.$Size)) != 1) # check whether the size of the school is the same for each unique ID
+            cat("Provided sizes for school with ID ",uID[i]," differ!\n")
+    }
+}
+## Result:
+## - No such case => 'Urban' and 'Rural' could indicate the school or the student.
+## - Also, 'Size' is indeed the same for each unique school (sanity check).
+
 
 ### 2 Save the cleaned data ####################################################
 
