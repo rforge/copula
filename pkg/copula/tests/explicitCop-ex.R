ea9d4a6b60f9468460194abf893064aed18cb323
mmaechler <mmaechler@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-07-28 17:05:33 +0000

    oops: need 'u' below
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/copula@1788 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/copula/tests/explicitCop-ex.R
+++ pkg/copula/tests/explicitCop-ex.R
@@ -122,8 +122,8 @@ U <- rCopula(100000, k.mC.g)
 ## check cdf with C.n
 
 ## Must work for d=1 [e.g. in 'gofCopula::gofKendallCvM()]:
-u <- as.matrix((0:7)/7)
-Cuu <- C.n(u,u) ## <- did not work for 1-dim. u[] matrix for a while
+u1 <- as.matrix((0:7)/7)
+Cuu <- C.n(u1,u1) ## <- did not work for 1-dim. u[] matrix for a while
 stopifnot(all.equal(Cuu, c(0:3,5:8)/8, tol = 1e-15))
 ## NOTE(MM): From the above result I think the finite-sample definition of C.n()
 ## ----      "sucks": It is not equidistant when u[] is.
